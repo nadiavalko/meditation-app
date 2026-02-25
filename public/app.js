ae8b0186993a49efe1b6fb99a0c924b67d55862f
NV <nvalkouskaya@gmail.com>
2026-02-25 13:38:00 -0800

    Increase body outline reveal delay further after body scan prompt

--- public/app.js
+++ public/app.js
@@ -310,7 +310,7 @@ if (burnInput && burnButton && burnFrame && burnTitle) {
               void journeyBodyFigure.offsetWidth;
               journeyBodyFigure.classList.add("is-revealing");
             }
-          }, finalStopRevealAt + revealDuration + holdAfterWellDoneMs + fadeOutDuration + 650);
+          }, finalStopRevealAt + revealDuration + holdAfterWellDoneMs + fadeOutDuration + 950);
         }, fadeOutDuration);
       };
     }
